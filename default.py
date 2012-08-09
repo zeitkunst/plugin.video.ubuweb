18971e1407a98983da4765a27c3a1469b150749e
Nicholas Knouf <nknouf@zeitkunst.org>
2012-08-09 19:53:12 -0400

    -- updating for release

--- default.py
+++ default.py
@@ -1,8 +1,20 @@
-# TODO
-# * Cleanup code, remove extraneous bits from tutorial code
-# * Put UbuWeb class file in correct place
-# * Write code to check to latest DB
-# * Be more careful with DB file download and writing
+# Copyright (C) 2012, Nicholas Knouf
+# 
+# This file is part of plugin.video.ubuweb
+#
+# plugin.video.ubuweb is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import sys
 import xbmc, xbmcgui, xbmcplugin
